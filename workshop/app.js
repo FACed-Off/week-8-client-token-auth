ccf2f4e7f8c13790bac80395ebdd5e6a8afc1078
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-10-22 15:36:21 +0100

    logging in

--- workshop/app.js
+++ workshop/app.js
@@ -19,6 +19,12 @@ function LoginForm() {
       id: "loginForm",
       onsubmit: (event) => {
         event.preventDefault();
+        //console.log(event);
+        const email = event.target.elements.email.value;
+        const password = event.target.elements.password.value;
+        login(email, password).then((user) => {
+          console.log(user);
+        });
       },
     },
     h("label", { for: "email" }, "Email"),
